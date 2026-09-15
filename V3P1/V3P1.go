46374cc4e17abbfac5ca6a0fc6740abe931130e0
Cristopher Baca <cristopher.baca@cetys.edu.mx>
2026-09-15 15:32:25 -0700

    .

--- V3P1/V3P1.go
+++ V3P1/V3P1.go
@@ -1,1 +1,17 @@
-package v3p1
+package main
+
+import "fmt"
+
+func main() {
+
+    segundos := 9385
+
+    horas := segundos / 3600
+    resto := segundos % 3600
+
+    minutos := resto / 60
+    segundosRestantes := resto % 60
+
+    fmt.Printf("Horas: %d, Minutos: %d, Segundos: %d\n",horas,minutos,segundosRestantes)
+}
+
--- V3P2/V3P2.go
+++ V3P2/V3P2.go
@@ -1,1 +1,17 @@
-package v3p2
+package main
+
+import "fmt"
+
+func main() {
+    a := 252
+    b := 105
+
+    for b != 0 {
+        resto := a % b
+        a = b
+        b = resto
+    }
+
+    fmt.Println("MCD:", a)
+}
+

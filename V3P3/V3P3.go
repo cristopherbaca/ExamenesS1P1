a180dfd8de10ece3ceaf5591d9d139363c0056ee
Cristopher Baca <cristopher.baca@cetys.edu.mx>
2026-09-16 13:39:23 -0700

    .

--- V3P3/V3P3.go
+++ V3P3/V3P3.go
@@ -1,6 +1,27 @@
 package main
 
-func main(){
+import "fmt"
 
-	arr[]
+func main() {
+	arr := []int{1, 2, 2, 3, 4, 4, 5, 5, 5, 7}
+
+	output := []int{}
+
+	for i := 0; i < len(arr); i++ {
+		encontrado := false
+
+		for j := 0; j < len(output); j++ {
+			if arr[i] == output[j] {
+				encontrado = true
+			}
+		}
+
+		if !encontrado {
+			output = append(output, arr[i])
+		}
+	}
+
+	for _, x := range output {
+		fmt.Println(x)
+	}
 }
